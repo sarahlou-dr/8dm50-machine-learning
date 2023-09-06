55b42195eed0e363fad59f4faf64854168e9b67f
sarahlou-dr <sarahlouderuiter@gmail.com>
2023-09-06 12:21:07 +0200

    linear reg changes

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -16,3 +16,22 @@ def lsq(X, y):
     beta = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
 
     return beta
+
+def mse(X, y_real, beta):
+
+    # add column of ones for the intercept
+    ones = np.ones((len(X), 1))
+    X = np.concatenate((ones, X), axis=1)
+
+    # calculate y estimates
+    y_est = np.matmul( beta.T,  X)  
+    print(y_est.shape)
+
+    y_est2 = np.dot(X, beta)
+
+    # calculate the mean squared error between estimates and real y values
+    error = (np.linalg.norm(y_real - y_est))**2 / y_real.shape[1]
+
+    error2 = (np.linalg.norm(y_real - y_est2))**2 / y_real.shape[1]
+
+    return error, error2
